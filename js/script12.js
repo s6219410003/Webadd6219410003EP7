fd8b907796bab7d53989f398b3b207a402228340
s6219410003 <s6219410003@sau.ac.th>
2021-11-04 23:45:00 +0700

    Code V4 : Do xxx

--- js/script12.js
+++ js/script12.js
@@ -1,36 +1,37 @@
-let weight = document.queryCommandIndeterm('weight')
-let height = document.queryCommandIndeterm('height')
-let calBMI = document.queryCommandIndeterm('calBMI')
-let bmivale = document.queryCommandIndeterm('bmivale')
-let bmiresult = document.queryCommandIndeterm('bmiresult')
+let weight = document.getElementById('weight')
+let height = document.getElementById('height')
+let calBMI = document.getElementById('calBMI')
+let bmiValue = document.getElementById('bmiValue')
+let bmiResult = document.getElementById('bmiResult')
 
-
-
-function calculateBMI(){
-    if( weight.valueOf.trim().length==0){
+function calculateBMI() {
+    if (weight.value.length == 0) {
         alert('ป้อนน้ำหนักด้วย')
-
-    }else if( weight.valueOf.trim().length==0){
+    } else if (height.value.length == 0) {
         alert('ป้อนส่วนสูงด้วย')
-}else{
-    let bmivaleShow = Nmber(weight.value)
-/((Number(height.value)/100)** 2)
-bmivale.innerHTML = bmivaleShow.toFixed(2)
+    } else {
+        //คำนวณ
+        let bmiValueShow = Number(weight.value) / ((Number(height.value) / 100) ** 2)
 
-//เอาค่า BMI มาแปลผล
-if(bmivaleShow<18.50){
-    bmiresult.innerHTML ='น้ำน้อย/ผอม'
-}else if( bmivaleShow <=22.90){
-    bmiresult.innerHTML ='ปกติ (สุขภาพดี)'
-}else if( bmivaleShow <=24.99){
-    bmiresult.innerHTML = 'ท้วม/โรคอ้วนระดับ 1'
-}else if( bmivaleShow <=29.99){
-    bmiresult.innerHTML = 'อ้วน/โรคอ้วนระดับ 2'
-}else if( bmivaleShow <=29.99){
-    bmiresult.innerHTML = 'อ้วนมาก/โรคอ้วนระดับ 3'
+        bmiValue.innerHTML = bmiValueShow.toFixed(2)
 
-calBMI.addEventListener('click',(calculateBMI))
+        //เอาค่า BMI มาแปลผล
+        if (bmiValueShow < 18.50) {
+            bmiResult.innerHTML = 'น้ำน้อย / ผอม'
+        } else if (bmiValueShow <= 22.90) {
+            bmiResult.innerHTML = 'ปกติ (สุขภาพดี)'
+        } else if (bmiValueShow <= 24.90) {
+            bmiResult.innerHTML = 'ท้วม / โรคอ้วนระดับ 1'
+        } else if (bmiValueShow <= 29.90) {
+            bmiResult.innerHTML = 'อ้วน / โรคอ้วนระดับ 2'
+        } else {
+            bmiResult.innerHTML = 'อ้วน / โรคอ้วนระดับ 3'
+        }
+    }
+}
 
-calBMI.addEventListener('mouseover',()=>{
-    bmiresult.innerHTML = 'SAU IoT DTI'
+calBMI.addEventListener('click', calculateBMI)
+
+calBMI.addEventListener('mouseover', ()=>{
+    bmiResult.innerHTML = 'SAU Iot DTI'
 })
